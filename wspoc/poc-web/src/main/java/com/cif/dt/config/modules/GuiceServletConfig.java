0fde317df14380a5b3dce05a72e438aaf4f3bd29
Josvic Zammit <jvzammit@gmail.com>
2013-02-19 16:12:26 +0100

    * fixed GuiceServletConfig as described in http://stackoverflow.com/a/14949229/1211429

--- wspoc/poc-web/src/main/java/com/cif/dt/config/modules/GuiceServletConfig.java
+++ wspoc/poc-web/src/main/java/com/cif/dt/config/modules/GuiceServletConfig.java
@@ -1,23 +1,40 @@
 package com.cif.dt.config.modules;
 
+import javax.servlet.ServletContextEvent;
+
 import com.cif.dt.app.websocket.MainSocket;
 import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.persist.PersistService;
+import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
 
 public class GuiceServletConfig extends GuiceServletContextListener {
 	
+	@Inject
+	PersistService ps;
+	
 	@Override
 	protected Injector getInjector() {
 		
-		return Guice.createInjector(new GuicePersistModule(), new ServletModule() {
+		Injector injector = Guice.createInjector(new ServletModule() {
 			
 			@Override
-		    protected void configureServlets() {								
+		    protected void configureServlets() {
+				install(new JpaPersistModule("DesktopPU"));
 				serve("/socket/main/").with(MainSocket.class);
 			}
 		
-		});
+		});		
+		injector.injectMembers(this);
+	    return injector;
+	}
+	
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+	    super.contextInitialized(servletContextEvent);
+	    ps.start();
 	}
 }
